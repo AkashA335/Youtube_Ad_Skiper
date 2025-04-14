1ed8fd60d40f289007ca3c3141d7fb72c188cbba
EAarthi <131673344+EAarthi@users.noreply.github.com>
2025-04-14 10:59:30 +0530

    Initial commit (V.1.0).
    Working code.

---
+++ ext/script.js
@@ -0,0 +1,13 @@
+
+async function updateTotalBlockedAdsDisplay() {
+    const counterElement = document.querySelector('.ads-skipped');
+    const totalBlocked =
+      (await chrome.storage.local.get(['totalBlocked'])).totalBlocked ?? 0;
+    counterElement.innerHTML = totalBlocked;
+  }
+  
+  function main() {
+    updateTotalBlockedAdsDisplay();
+  }
+  
+  main();
